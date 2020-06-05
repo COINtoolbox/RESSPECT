fix last mag calculation

resspect/database.py
@@ -579,8 +579,7 @@ class DataBase:
                                  "\n Feel free to add other options.")
 
     def build_random_training(self, initial_training: int, nclass=2, screen=False,
-                              Ia_frac=0.5, queryable=True, sep_files=False,
-                              sep_validation=False):
+                              Ia_frac=0.5, queryable=True, sep_files=False):
         """Construct initial random training and corresponding test sample.
 
         Populate properties: train_features, train_header, test_features,
@@ -614,10 +613,8 @@ class DataBase:
         id_name = self.identify_keywords()
 
         if sep_files:
-            # build complete metadata object
-            self.metadata = pd.concat([self.train_metadata, self.test_metadata])
-            self.features = np.concatenate((self.train_features, self.test_features))
-
+            pass
+        
         # identify Ia
         data_copy = self.metadata.copy()
         ia_flag = data_copy['type'] == 'Ia'
@@ -639,22 +636,8 @@ class DataBase:
         self.train_metadata = data_copy[train_flag]
         self.train_features = self.features[train_flag].values
 
-        if sep_validation:
-            # get all ids which are not in training 
-            all_not_train_ids = data_copy[id_name].values[~train_flag]
-
-            # select 25% of the non-train ids, via their indexes
-            indx = np.random.randint(low=0, high=len(all_not_train_ids), 
-                                     size=int(0.25 * len(all_not_train_ids)))
-            validation_flag = np.array([obj in all_not_train_ids[indx] 
-                                        for obj in data_copy[id_name].values])
-            test_flag = np.logical_and(~train_flag, ~validation_flag)
-              
-            self.validation_metadata = data_copy[validation_flag]
-            self.validation_features = self.features[validation_flag].values
-            self.test_metadata = data_copy[test_flag]
-            self.test_features = self.features[test_flag].values
- 
+        if sep_files:
+            pass
         else:
             # get test sample
             test_flag = ~train_flag
@@ -662,28 +645,20 @@ class DataBase:
             self.test_features = self.features[test_flag].values
             self.pool_metadata = self.test_metadata
             self.pool_features = self.test_features
+            self.validation_features = self.test_features
+            self.validation_metadata = self.test_metadata
 
-        if nclass == 2:
             self.train_labels = data_copy['type'][train_flag].values == 'Ia'
             self.test_labels = data_copy['type'][test_flag].values == 'Ia'
-          
-            if sep_validation:
-                self.validation_labels = data_copy['type'][validation_flag].values == 'Ia'
+            self.pool_labels = self.test_labels
+            self.validation_labels = self.test_labels
+        
+            if queryable:
+                queryable_flag = data_copy['queryable'].values
+                combined_flag = np.logical_and(~train_flag, queryable_flag)
+                self.queryable_ids = data_copy[combined_flag][id_name].values
             else:
-                self.pool_labels = self.test_labels
-        else:
-            raise ValueError("Only 'Ia x non-Ia' are implemented! "
-                             "\n Feel free to add other options.")
-
-        if queryable and not sep_validation:
-            queryable_flag = data_copy['queryable'].values
-            combined_flag = np.logical_and(test_flag, queryable_flag)
-            self.queryable_ids = data_copy[combined_flag][id_name].values
-
-        elif sep_validation:
-            self.queryable_ids = data_copy[test_flag][id_name].values
-        else:
-            self.queryable_ids = self.test_metadata[id_name].values
+                self.queryable_ids = self.test_metadata[id_name].values
 
         # check if there are repeated ids
         train_test_flag = np.array([True if item in self.test_metadata[id_name].values
@@ -781,7 +756,7 @@ class DataBase:
                       screen=False, Ia_frac=0.5,
                       queryable=False, save_samples=False, sep_files=False,
                       survey='DES', output_fname=' ', path_to_train=' ',
-                      path_to_queried=' ', method='Bazin', sep_validation=False):
+                      path_to_queried=' ', method='Bazin'):
         """Separate train, test and validation samples.
 
         Populate properties: train_features, train_header, test_features,
@@ -830,8 +805,6 @@ class DataBase:
         sep_files: bool (optional)
             If True, consider train and test samples separately read
             from independent files. Default is False.
-        sep_validation: bool (optional)
-            If True, construt separated validation sample. Default is False.
         """
 
         if initial_training == 'original':
@@ -847,8 +820,7 @@ class DataBase:
             self.build_random_training(initial_training=initial_training,
                                        nclass=nclass, screen=screen,
                                        Ia_frac=Ia_frac, queryable=queryable,
-                                       sep_files=sep_files, 
-                                       sep_validation=sep_validation)
+                                       sep_files=sep_files)
 
         if screen:
             print('Training set size: ', self.train_metadata.shape[0])
@@ -1166,7 +1138,7 @@ class DataBase:
         else:
             raise ValueError('Invalid strategy.')
 
-    def update_samples(self, query_indx: list, loop: int, epoch=0,
+    def update_samples(self, query_indx: list, epoch=20,
                        queryable=False):
         """Add the queried obj(s) to training and remove them from test.
 
@@ -1177,10 +1149,8 @@ class DataBase:
         ----------
         query_indx: list
             List of indexes identifying objects to be moved.
-        loop: int
-            Store number of loop when this query was made.
         epoch: int (optional)
-            Day of initial loop. Default is 0.
+            Day since beginning of survey. Default is 20.
         queryable: bool (optinal)
             If True, consider queryable flag. Default is False.
         """

resspect/time_domain_loop.py
@@ -15,20 +15,75 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__all__ = ['time_domain_loop']
+__all__ = ['time_domain_loop', 'load_dataset']
 
 import numpy as np
 import pandas as pd
 
 from resspect import DataBase
 
+def load_dataset(fname: str, features_method='Bazin', survey='DES',
+                         screen=False, initial_training='original',
+                         ia_frac=0.5, queryable=False):
+    """Read a data sample from file.
+    
+    Parameters
+    ----------
+    fname: str #or dict
+        Path to light curve features file.
+        #if "sep_files == True", dictionary keywords must contain identify
+        #different samples: ['train', 'test','validation', 'pool']
+    features_method: str (optional)
+        Feature extraction method. Currently only 'Bazin' is implemented.
+    ia_frac: float in [0,1] (optional)
+        Fraction of Ia required in initial training sample.
+        Only used if "initial_training" is a number. Default is 0.5.
+    initial_training: str or int (optional)
+        Choice of initial training sample.
+        If 'original': begin from the train sample flagged in the file
+        elif int: choose the required number of samples at random,
+        ensuring that at least "ia_frac" are SN Ia.
+        Default is 'original'.    
+    queryable: bool (optional)
+        If True, allow queries only on objects flagged as queryable.
+        Default is True.
+    screen: bool (optional)
+        If True, print on screen number of light curves processed.
+    survey: str (optional)
+        Name of survey to be analyzed. Accepts 'DES' or 'LSST'.
+        Default is DES.
+    
+    Returns
+    -------
+    resspect.DataBase object
+    """
+
+    # initiate object
+    data = DataBase()
+
+    # constructs training, test, validation and pool samples
+    data.load_features(fname, method=features_method,
+                       screen=screen, survey=survey)
+    
+     # get identification keyword
+    id_name = data.identify_keywords()
+    
+    data.build_samples(initial_training=initial_training, nclass=2,
+                       screen=screen, Ia_frac=ia_frac,
+                       queryable=queryable, save_samples=False,
+                       sep_files=None, survey=survey, 
+                       method=features_method)
+    
+    return data
+    
+    
 def time_domain_loop(days: list,  output_metrics_file: str,
                      output_queried_file: str,
                      path_to_features_dir: str, strategy: str,
                      fname_pattern: list,
                      batch=1, canonical = False,  classifier='RandomForest',
                      cont=False, first_loop=20, features_method='Bazin', nclass=2,
-                     Ia_frac=0.5, output_fname="", path_to_canonical="",
+                     ia_frac=0.5, output_fname="", path_to_canonical="",
                      path_to_ini_train="", path_to_train="",
                      path_to_queried="", queryable=True,
                      query_thre=1.0, save_samples=False, sep_files=False,
@@ -56,38 +111,21 @@ def time_domain_loop(days: list,  output_metrics_file: str,
         Size of batch to be queried in each loop. Default is 1.
     canonical: bool (optional)
         If True, restrict the search to the canonical sample.
-    continue: bool (optional)
-        If True, read the initial states of previous runs from file.
-        Default is False.
     classifier: str (optional)
         Machine Learning algorithm.
         Currently 'RandomForest', 'GradientBoostedTrees',
         'KNN', 'MLP', 'SVM' and 'NB' are implemented.
         Default is 'RandomForest'.
-    first_loop: int (optional)
-        First day of the survey already calculated in previous runs.
-        Only used if initial_training == 'previous'.
-        Default is 20.
     features_method: str (optional)
         Feature extraction method. Currently only 'Bazin' is implemented.
-    Ia_frac: float in [0,1] (optional)
+    ia_frac: float in [0,1] (optional)
         Fraction of Ia required in initial training sample.
         Default is 0.5.
-    nclass: int (optional)
-        Number of classes to consider in the classification
-        Currently only nclass == 2 is implemented.
     path_to_canonical: str (optional)
         Path to canonical sample features files.
         It is only used if "strategy==canonical".
     path_to_ini_train: str (optional)
         Path to full light curve features file.
-        Only used if "training == 'original'".
-    path_to_train: str (optional)
-        Path to initial training file from previous run.
-        Only used if initial_training == 'previous'.
-    path_to_queried: str(optional)
-        Path to queried sample from previous run.
-        Only used if initial_training == 'previous'.
     queryable: bool (optional)
         If True, allow queries only on objects flagged as queryable.
         Default is True.
@@ -116,7 +154,7 @@ def time_domain_loop(days: list,  output_metrics_file: str,
         Only used if save_samples == True.
     """
 
-    # initiate object
+    """# initiate object
     data = DataBase()
 
     # constructs training, test, validation and pool samples
@@ -127,50 +165,60 @@ def time_domain_loop(days: list,  output_metrics_file: str,
     id_name = data.identify_keywords()
     
     data.build_samples(initial_training=initial_training, nclass=nclass,
-                       screen=screen, Ia_frac=Ia_frac,
+                       screen=screen, Ia_frac=ia_frac,
                        queryable=queryable, save_samples=save_samples,
                        sep_files=sep_files, survey=survey, 
                        output_fname=output_fname,
                        path_to_train=path_to_train,
                        path_to_queried=path_to_queried,
                        method=features_method)
+    """
+
+    # read initial training
+    data = load_dataset(fname=path_to_ini_train, 
+                        features_method=features_method,
+                        survey=survey,
+                        screen=screen,
+                        initial_training=initial_training,
+                        ia_frac=ia_frac, queryable=queryable)
     
     # load features for the first obs day
-    path_to_features3 = path_to_features_dir + fname_pattern[0] + \
+    path_to_first_loop = path_to_features_dir + fname_pattern[0] + \
                                        str(days[0]) + fname_pattern[1]
     
-    first_loop = DataBase()
-    first_loop.load_features(path_to_features3)
-    first_loop.build_samples(initial_training=initial_training, 
-                             nclass=nclass,
-                             screen=screen, Ia_frac=Ia_frac,
-                             queryable=queryable, 
-                             save_samples=save_samples,
-                             sep_files=sep_files, survey=survey,
-                             output_fname=output_fname,
-                             path_to_train=path_to_train,
-                             path_to_queried=path_to_queried,
-                             method=features_method)
-            
-    # update test and pool sample
-    data.pool_features = first_loop.pool_features
-    data.pool_labels = first_loop.pool_labels
-    data.pool_metadata = first_loop.pool_metadata
+    # read data from fist loop
+    first_loop = load_dataset(fname=path_to_first_loop, 
+                              features_method=features_method,
+                              survey=survey,
+                              screen=screen,
+                              initial_training=0,
+                              ia_frac=ia_frac, queryable=queryable)
     
+    # get keyword for obj identification
+    id_name = data.identify_keywords()
+    
+    # update test and pool sample    
     if sep_files:
         pass
     else:
+        data.pool_features = first_loop.pool_features
+        data.pool_metadata = first_loop.pool_metadata
+        data.pool_labels = first_loop.pool_labels
+        
         data.test_features = first_loop.features
+        data.test_metadata = first_loop.metadata
         test_labels = first_loop.metadata['type'].values == 'Ia'
         data.test_labels = test_labels.astype(int)
-        data.test_metadata = first_loop.metadata
+        
         data.validation_features = first_loop.features
         data.validation_metadata = first_loop.metadata
         data.validation_labels = data.test_labels
                 
     if queryable:
         q_flag = data.pool_metadata['queryable'].values
-        data.queryable_ids = data.pool_metadata['id'].values[q_flag]
+        data.queryable_ids = data.pool_metadata[id_name].values[q_flag]
+    else:
+        data.queryable_ids = data.pool_metadata[id_name].values
 
     # get list of canonical ids
     if canonical:
@@ -191,12 +239,6 @@ def time_domain_loop(days: list,  output_metrics_file: str,
         if screen:
             print('Processing night: ', night)
 
-        # cont loop
-        if initial_training == 'previous':
-            loop = night - first_loop
-        else:
-            loop = night - int(days[0])
-
         if data.pool_metadata.shape[0] > 0:
             # classify
             data.classify(method=classifier, **kwargs)
@@ -204,80 +246,87 @@ def time_domain_loop(days: list,  output_metrics_file: str,
             # calculate metrics
             data.evaluate_classification()
         
+            # get index of object to be queried
             indx = data.make_query(strategy=strategy, batch=batch, 
                                    queryable=queryable,
                                    query_thre=query_thre)
 
             # update training and test samples
-            data.update_samples(indx, loop=loop, queryable=queryable, 
-                                epoch=loop)
+            data.update_samples(indx, queryable=queryable, 
+                                epoch=night)
             
             if screen:
                 print('After update_samples:')
                 print('   ... train: ', data.train_metadata.shape[0])
+                print('   ... test: ', data.test_metadata.shape[0])
+                print('   ... validation: ', data.validation_metadata.shape[0])
                 print('   ... pool: ', data.pool_metadata.shape[0])
+                
 
-        # save metrics for current state
-        data.save_metrics(loop=loop, output_metrics_file=output_metrics_file,
-                          batch=batch, epoch=night)
+            # save metrics for current state
+            data.save_metrics(loop=night - days[0], output_metrics_file=output_metrics_file,
+                              batch=batch, epoch=night)
 
-        # save query sample to file
-        data.save_queried_sample(output_queried_file, loop=loop,
+            # save query sample to file
+            data.save_queried_sample(output_queried_file, loop=night - days[0],
                                      full_sample=False)
 
-        # load features for next day
-        path_to_features2 = path_to_features_dir + fname_pattern[0] + \
-                            str(night + 1) + fname_pattern[1]
+            # load features for next day
+            path_to_features2 = path_to_features_dir + fname_pattern[0] + \
+                                str(night + 1) + fname_pattern[1]
 
-        data_tomorrow = DataBase()
-        data_tomorrow.load_features(path_to_features2, method=features_method,
-                                    screen=screen, survey=survey)
+            data_tomorrow = load_dataset(fname=path_to_features2, 
+                              features_method=features_method,
+                              survey=survey,
+                              screen=screen,
+                              initial_training=0,
+                              ia_frac=ia_frac, queryable=queryable)
 
-        # identify objects in the new day which must be in training
-        train_flag = np.array([item in data.train_metadata[id_name].values 
-                              for item in data_tomorrow.metadata[id_name].values])
-        train_ids = data_tomorrow.metadata[id_name].values[train_flag]
+            # identify objects in the new day which must be in training
+            train_flag = np.array([item in data.train_metadata[id_name].values 
+                                  for item in data_tomorrow.metadata[id_name].values])
+            train_ids = data_tomorrow.metadata[id_name].values[train_flag]
         
-        # keep objs who were in training but are not in the new day
-        keep_flag = np.array([item not in train_ids 
-                              for item in data.train_metadata[id_name].values])
+            # keep objs who were in training but are not in the new day
+            keep_flag = np.array([item not in train_ids 
+                                  for item in data.train_metadata[id_name].values])
         
-        # use new data for training (this might have extra obs points)
-        data.train_metadata = pd.concat([data.train_metadata[keep_flag],
+            # use new data for training (this might have extra obs points)
+            data.train_metadata = pd.concat([data.train_metadata[keep_flag],
                                          data_tomorrow.metadata[train_flag]])
-        data.train_features = np.append(data.train_features[keep_flag],
-                                        data_tomorrow.features[train_flag],
-                                        axis=0)
+            data.train_features = np.append(data.train_features[keep_flag],
+                                            data_tomorrow.features[train_flag],
+                                            axis=0)
          
-        train_labels = data.train_metadata['type'].values == 'Ia'
-        data.train_labels = train_labels.astype(int)
+            train_labels = data.train_metadata['type'].values == 'Ia'
+            data.train_labels = train_labels.astype(int)
 
-        # use new data
-        if sep_files:
-            pass
-        else:
-            data.pool_metadata = data_tomorrow.metadata[~train_flag]
-            data.pool_features = data_tomorrow.features.values[~train_flag]
-            data.validation_features = data.pool_features
-            data.validation_metadata = data.pool_metadata
+            # use new data
+            if sep_files:
+                pass
+            else: 
+                data.pool_metadata = data_tomorrow.metadata[~train_flag]
+                data.pool_features = data_tomorrow.features.values[~train_flag]
+                data.validation_features = data.pool_features
+                data.validation_metadata = data.pool_metadata
             
-            pool_labels = data.pool_metadata['type'].values == 'Ia'
-            data.pool_labels = pool_labels.astype(int)
-            data.validation_labels = data.pool_labels
+                pool_labels = data.pool_metadata['type'].values == 'Ia'
+                data.pool_labels = pool_labels.astype(int)
+                data.validation_labels = data.pool_labels
 
-        if strategy == 'canonical':
-            data.queryable_ids = canonical.queryable_ids
+            if strategy == 'canonical':
+                data.queryable_ids = canonical.queryable_ids
 
-        if  queryable:
-            queryable_flag = data.pool_metadata['queryable'].values
-            data.queryable_ids = data.pool_metadata['id'].values[queryable_flag]
-        else:
-            data.queryable_ids = data.pool_metadata['id'].values
+            if  queryable:
+                queryable_flag = data.pool_metadata['queryable'].values
+                data.queryable_ids = data.pool_metadata['id'].values[queryable_flag]
+            else:
+                data.queryable_ids = data.pool_metadata['id'].values
 
-        if screen:
-            print('Training set size: ', data.train_metadata.shape[0])
-            print('Test set size: ', data.test_metadata.shape[0])
-            print('Queryable set size: ', len(data.queryable_ids))
+            if screen:
+                print('Training set size: ', data.train_metadata.shape[0])
+                print('Test set size: ', data.test_metadata.shape[0])
+                print('Queryable set size: ', len(data.queryable_ids))
 
 
 def main():