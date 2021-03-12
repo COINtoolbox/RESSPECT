b3790ed81951ea2d0f4c4e03fbdd57223d3b2f7a
Kara Ponder <kara@clrlsstpc02.in2p3.fr>
2021-03-12 20:25:18 +0100

    added fish_deriv_m tests

--- resspect/tests/test_cosmo_metric_utils.py
+++ resspect/tests/test_cosmo_metric_utils.py
@@ -36,3 +36,15 @@ def test_assign_cosmo():
     assert hasattr(updated_cosmo, 'distmod')
     assert isinstance(updated_cosmo, w0waCDM)
 
+def test_fish_deriv_m():
+    from resspect import cosmo_metric_utils as cmu
+
+    redshift = np.arange(0.2, 1.5, 0.1)
+    step = np.array([0, 0.001, 0.00, 0.1, 0., 0.0, 0.0, 0.0])
+    m, m_deriv = cmu.fish_deriv_m(redshift, [72, 0.29, 0.71, -1, 0.0], step)
+
+    assert np.shape(m) == np.shape(redshift)
+    assert len(m_deriv) > 0
+
+    assert not np.isnan(m).any()
+    assert not np.isnan(m_deriv).any()
