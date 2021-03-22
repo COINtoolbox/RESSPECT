108eedc4deb77f276e5c697747210580610fc2d8
emilleishida <emilleishida@gmail.com>
2021-03-22 23:51:47 +0100

    add cost in 4 and 8m telescope for plasticc

--- resspect/database.py
+++ resspect/database.py
@@ -283,6 +283,13 @@ class DataBase:
             elif 'id' in data.keys():
                 self.metadata_names = ['id', 'redshift', 'type', 'code',
                                        'orig_sample', 'queryable']
+                
+            if 'last_rmag' in data.keys():
+                self.metadata_names.append('last_rmag')
+
+            for name in self.telescope_names:
+                if 'cost_' + name in data.keys():
+                    self.metadata_names = self.metadata_names + ['cost_' + name]
 
         else:
             raise ValueError('Only "DES" and "LSST" filters are ' + \
