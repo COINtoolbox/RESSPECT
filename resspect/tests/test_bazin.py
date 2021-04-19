d93a4c29b7fbd373ec86be9f9d7f8a2039f5da2f
Bruno Quint <b1quint@gmail.com>
2021-04-19 10:39:46 -0400

    Enable GitHub Actions for this branch

---
+++ resspect/tests/test_bazin.py
@@ -0,0 +1,81 @@
+"""
+Tests related to bazin.py module.
+"""
+
+import numpy as np
+import os
+import pytest
+
+from pandas import read_csv
+
+from resspect import testing
+
+
+def test_bazin():
+    """
+    Test the Bazin function evaluation.
+    """
+
+    from resspect import bazin
+    
+    time = 3
+    a = 1
+    b = 1
+    t0 = 10
+    tfall = 3
+    trise = 4
+    
+    res = bazin(time, a, b, t0, tfall, trise)
+    
+    assert not np.isnan(res).any()
+
+
+def test_errfunc():
+    """
+    Test the error between calculates and observed error.
+    """
+
+    from resspect import bazin, errfunc
+    
+    # input for bazin
+    time = np.arange(0, 50, 3.5)
+    a = 1
+    b = 1
+    t0 = 10
+    tfall = 3
+    trise = 4
+    
+    # calculate fiducial flux values
+    flux_fid = bazin(time, a, b, t0, tfall, trise)
+    
+    # add noise
+    flux = [np.random.normal(loc=item, scale=0.01) for item in flux_fid]
+    
+    # construct parameters vector
+    params = [a, b, t0, tfall, trise]
+
+    res = errfunc(params, time, flux)
+    
+    assert not np.isnan(res).any()
+    assert np.all(res > 0)
+    
+    
+def test_fit_scipy():
+    """
+    Test the scipy fit to Bazin parametrization.
+    """
+    from resspect import fit_scipy
+    
+    fname = testing.download_data('tests/lc_mjd_flux.csv')
+    data = read_csv(fname)
+    
+    time = data['mjd'].values
+    flux = data['flux'].values
+    
+    res = fit_scipy(time, flux)
+    
+    assert not np.isnan(res).any()
+
+
+if __name__ == '__main__':
+    pytest.main()
