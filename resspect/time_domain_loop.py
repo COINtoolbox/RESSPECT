ca343713fd825a61bcc96520ba8db35185717188
AmandaWasserman <awasserm@u.rochester.edu>
2024-05-29 12:17:35 -0500

    TOM updates

--- resspect/time_domain_loop.py
+++ resspect/time_domain_loop.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__all__ = ['time_domain_loop', 'load_dataset']
+__all__ = ['time_domain_loop', 'load_dataset', 'submit_queries_to_TOM']
 
 import os
 from typing import Union, Tuple
@@ -24,6 +24,7 @@ import pandas as pd
 import progressbar
 
 from resspect import DataBase
+from resspect.tom_client import TomClient
 
 
 def load_dataset(file_names_dict: dict, survey_name: str = 'DES',
@@ -799,11 +800,12 @@ def process_next_day_loop(
     return light_curve_data
 
 
-def submit_queries_to_TOM(objectids: list, priorities: list, requester: str='resspect'):
+def submit_queries_to_TOM(username, passwordfile, objectids: list, priorities: list, requester: str='resspect'):
+    tom = TomClient(url = "https://desc-tom-2.lbl.gov", username = username, passwordfile = passwordfile)
     req = { 'requester': requester,
             'objectids': objectids,
             'priorities': priorities}
-    res = TomClient.request( 'POST', 'elasticc2/askforspectrum', json=req )
+    res = tom.request( 'POST', 'elasticc2/askforspectrum', json=req )
     dic = res.json()
     if res.satus_code != 200:
         raise ValueError('Request failed, ' + res.text + ". Status code: " + str(res.status_code))
