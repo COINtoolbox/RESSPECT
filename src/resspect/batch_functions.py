444e421afc80adb988d68dc7353a3c308155af56
Michael Tauraso <mtauraso@uw.edu>
2024-10-31 12:27:26 -0700

    Removing erronious GNU GPL notifications from the source tree. - This was missed when the LICENCE file was changed to MIT - Collecting information from Amanda, Emille, Rob, and Alex I believe   the notices to be in error, and the actual status of the codebase at   time of writing is that the entirety of it is MIT licensed.

--- src/resspect/batch_functions.py
+++ src/resspect/batch_functions.py
@@ -3,12 +3,6 @@
 #
 # created on 26 June 2020
 #
-# Licensed GNU General Public License v3.0;
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.gnu.org/licenses/gpl-3.0.en.html
-#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- src/resspect/exposure_time_calculator.py
+++ src/resspect/exposure_time_calculator.py
@@ -7,13 +7,6 @@
 # modified public DECam exposure time calculator by
 # CC F. Forster, J. Martinez, J.C. Maureira, 
 # https://github.com/fforster/HiTS-public
-# 
-#
-# Licensed GNU General Public License v3.0;
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.gnu.org/licenses/gpl-3.0.en.html
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
